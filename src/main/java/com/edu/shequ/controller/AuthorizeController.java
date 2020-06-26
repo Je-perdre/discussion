f79ae2dc391d05f5f8150d21a1806bf905ed4622
Je-perdre <954886703@qq.com>
2020-06-26 13:15:13 +0800

    push2

---
+++ src/main/java/com/edu/shequ/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package com.edu.shequ.controller;
+
+import com.edu.shequ.dto.AccessTokenDTO;
+import com.edu.shequ.dto.GithubUser;
+import com.edu.shequ.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//@PropertySource("classpath:application.properties")
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    private String callback(@RequestParam(name = "code") String code,
+                            @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        String accessToken= githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user=githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/edu/shequ/provider/GithubProvider.java
@@ -0,0 +1,64 @@
+package com.edu.shequ.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.edu.shequ.dto.AccessTokenDTO;
+import com.edu.shequ.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+
+
+//@Component紧紧的把当前类初始化到spring服务器的上下文
+
+/**
+ * //@Controller将当前的类作为路由api的承载者
+ * //避免使用以下  spring ioc功能强大之处
+ * public class GithubProvider {
+ * public static void main(String[] args){
+ * GithubProvider githubProvider =new GithubProvider();
+ * }
+ * }
+ */
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        /*实现获取git用户信息access_token=4574e7e9c2c3b47772b84b6f8bbf5b10cabd7dbb&scope=user&token_type=bearer
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            System.out.println(string);
+            return string;
+         */
+        /*获取优化  获取：小谈在线*/
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            return string.split("&")[0].split("=")[1];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            return JSON.parseObject(string, GithubUser.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
