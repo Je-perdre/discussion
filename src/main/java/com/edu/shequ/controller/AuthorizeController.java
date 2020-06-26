1e0fcd2229b00ff3a600aa5b17f890b09d3a12f5
Je-perdre <954886703@qq.com>
2020-06-26 14:13:03 +0800

    push2

--- src/main/java/com/edu/shequ/controller/AuthorizeController.java
+++ src/main/java/com/edu/shequ/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 //@PropertySource("classpath:application.properties")
 @Controller
 public class AuthorizeController {
@@ -27,7 +29,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     private String callback(@RequestParam(name = "code") String code,
-                            @RequestParam(name = "state") String state) {
+                            @RequestParam(name = "state") String state,
+                                    HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -36,8 +39,16 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken= githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user=githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+
+        if (user!=null){
+            //登录成功，写cookie 和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            //登录失败，重新登录
+            return "redirect:/";
+
+        }
     }
 
 }
